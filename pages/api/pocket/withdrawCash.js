a430a6c904e34d9d2b60517eab3b426a1236156c
arrijalifal <ifalgantengifal@gmail.com>
2023-06-29 22:45:56 +0700

    change pockethistory value to 'penarikan'

--- pages/api/pocket/withdrawCash.js
+++ pages/api/pocket/withdrawCash.js
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
                 pockethistory: db.util.append({
                     date,
                     type: 'income',
-                    name: '',
+                    name: 'Penarikan',
                     amount
                 })
             }, key);
