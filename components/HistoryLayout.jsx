af7aacb55be20e6da1031850c222edf888ede1b6
arrijalifal <ifalgantengifal@gmail.com>
2023-07-02 06:45:50 +0700

    change flex HistoryLayout

--- components/HistoryLayout.jsx
+++ components/HistoryLayout.jsx
@@ -1,15 +1,40 @@
 import React from "react";
+import { useState } from "react";
+import axios from "axios";
+
 
 export default function HistoryLayout({ balance, history, contentType }) {
-    console.log(balance);
+    const [customValue, setCustomValue] = useState('');
+
+    async function handleCustomValue(e) {
+        if (customValue < 0 && customValue === NaN) return;
+        const type = e.target.value;
+        const date = new Date();
+        const amount = parseInt(customValue)
+        const data = {
+            date: date.toISOString(),
+            amount,
+            type,
+        }
+        const result = await axios({
+            method: 'POST',
+            url: `${process.env.APP_URL}/api/pocket/handleMoneyDetail`,
+            data: {
+                ...data,
+                contentType,
+                key: session.user.email,
+            }
+        });
+    }
+
     return (
         <div id="maindiv" className="w-full h-3/4 flex flex-col">
             <section id="first-section" className='w-full p-3 rounded-t-2xl bg-[#4942E4] flex justify-between'>
                 <p className="inline">{contentType}</p>
                 <p className="inline">{balance} <span className="inline-block tracking-tight rotate-90">{'>>'}</span></p>
             </section>
-            <section id="second-section" className={`w-full ${(contentType === 'Tabungan') ? 'bg-[#11009E]' : 'bg-[#8696FE]'} rounded-b-2xl flex flex-col overflow-y-auto`}>
-                <div id="accounthistory" className="overflow-y-auto p-3">
+            <section id="second-section" className={`w-full ${(contentType === 'Tabungan') ? 'bg-[#11009E]' : 'bg-[#8696FE]'} rounded-b-2xl flex flex-col overflow-y-auto flex-1`}>
+                <div id="accounthistory" className="overflow-y-auto p-3 flex-1">
                     {
                         (history.length > 0) ?
                             history.map(ac => {
@@ -35,10 +60,10 @@ export default function HistoryLayout({ balance, history, contentType }) {
                     }
                 </div>
                 <div id="buttondiv" className="p-3 flex justify-center flex-col">
-                    <input type="text" className="block text-center focus:outline focus:outline-blue-700 p-2 rounded-xl mb-2" placeholder="Add Custom Value" />
+                    <input type="text" className="block text-center focus:outline focus:outline-blue-700 p-2 rounded-xl mb-2" placeholder="Add Custom Value" value={customValue} onChange={e => setCustomValue(e.target.value)} />
                     <div className="w-full flex justify-between">
-                        <button className=" w-[49%] py-2 rounded-xl bg-[#22F500]">Income</button>
-                        <button className=" w-[49%] py-2 rounded-xl bg-[#FF3636]">Expense</button>
+                        <button className=" w-[49%] py-2 rounded-xl bg-[#22F500]" value={'income'} onClick={handleCustomValue}>Income</button>
+                        <button className=" w-[49%] py-2 rounded-xl bg-[#FF3636]" value={'expense'} onClick={handleCustomValue}>Expense</button>
                     </div>
                 </div>
             </section>
